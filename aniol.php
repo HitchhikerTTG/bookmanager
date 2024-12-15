930e4c5b5a85c36f11ee92e2088ff4433ebff6ae
HitchhikerTTG <118632325+HitchhikerTTG@users.noreply.github.com>
2024-12-15 18:43:59 +0000

    Assistant checkpoint
    Assistant generated file changes:
    - aniol.php: Add JS and modal template
    
    ---
    
    User prompt:
    
    Dobra, teraz potrzebuję skupić się na tym, co się dzieje po kliknięciu w "add metadata"

--- aniol.php
+++ aniol.php
@@ -25,6 +25,9 @@ $manager = new BookManager();
         <?php include 'templates/library.php'; ?>
     </div>
     
+    <?php include 'templates/modals.php'; ?>
+    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="js/bookEditor.js"></script>
 </body>
 </html>
