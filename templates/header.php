d0eb7cddd2220afae2e328f0006e51cface11dfb
HitchhikerTTG <118632325+HitchhikerTTG@users.noreply.github.com>
2024-12-15 21:20:58 +0000

    Assistant checkpoint: Added responsive design improvements for different screens
    Assistant generated file changes:
    - templates/library.php: Improve form responsiveness
    - templates/header.php: Make header responsive
    
    ---
    
    User prompt:
    
    Czy możesz dodać poprawiona obsługe dla różnej wielkości ekranów?

--- templates/header.php
+++ templates/header.php
@@ -2,7 +2,7 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
         <a class="navbar-brand" href="#">Book Manager v<?php echo SCRIPT_VERSION; ?></a>
-        <div class="d-flex text-white">
+        <div class="d-flex flex-wrap text-white">
             <div class="me-3">Total Books: <?php echo $stats['total']; ?></div>
             <div class="me-3">With Metadata: <?php echo $stats['withMetadata']; ?></div>
             <div>Last Update: <?php echo $stats['lastUpdate']; ?></div>
--- templates/library.php
+++ templates/library.php
@@ -1,4 +1,3 @@
-
 <div class="table-responsive">
     <table class="table table-striped">
         <thead>
@@ -83,12 +82,12 @@
                         <input type="hidden" id="available-genres-<?php echo $rowId; ?>" value='<?php echo json_encode($manager->getLists()['genres']); ?>'>
                         <input type="hidden" id="available-authors-<?php echo $rowId; ?>" value='<?php echo json_encode($manager->getLists()['authors']); ?>'>
                         <input type="hidden" id="available-series-<?php echo $rowId; ?>" value='<?php echo json_encode($manager->getLists()['series']); ?>'>
-                        <div class="row">
-                            <div class="col-md-6 mb-3">
+                        <div class="row g-3">
+                            <div class="col-12 col-md-6 mb-3">
                                 <label class="form-label">Title</label>
                                 <input type="text" class="form-control" name="title" value="<?php echo $isProcessed ? htmlspecialchars($bookData['title']) : ''; ?>" required>
                             </div>
-                            <div class="col-md-6 mb-3">
+                            <div class="col-12 col-md-6 mb-3">
                                 <label class="form-label">Genres</label>
                                 <input type="text" class="form-control" name="genres" id="genres-<?php echo $rowId; ?>" 
                                        value="<?php echo $isProcessed ? htmlspecialchars(implode(', ', $bookData['genres'])) : ''; ?>" 
@@ -97,13 +96,13 @@
                         </div>
                         <div id="authors-container-<?php echo $rowId; ?>"></div>
                         <button type="button" class="btn btn-secondary btn-sm mt-2" onclick="addAuthor()">Add Author</button>
-                        <div class="row mt-3">
-                            <div class="col-md-6 mb-3">
+                        <div class="row g-3 mt-3">
+                            <div class="col-12 col-md-6 mb-3">
                                 <label class="form-label">Series</label>
                                 <input type="text" class="form-control" name="series" id="series-<?php echo $rowId; ?>" 
                                        value="<?php echo $isProcessed ? htmlspecialchars($bookData['series'] ?? '') : ''; ?>">
                             </div>
-                            <div class="col-md-6 mb-3">
+                            <div class="col-12 col-md-6 mb-3">
                                 <label class="form-label">Series Position</label>
                                 <input type="number" class="form-control" name="series_position" 
                                        value="<?php echo $isProcessed ? htmlspecialchars($bookData['series_position'] ?? '') : ''; ?>">
